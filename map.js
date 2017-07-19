8da90a8d8b79b6038ea40906d783015151ec22ce
rachelehunter <rachelehunter2001@gmail.com>
2017-07-19 10:43:35 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-var mapboxAccessToken = {your access token here};
+var mapboxAccessToken = {sk.eyJ1IjoicmFjaGVsaHVudGVyMjAwMSIsImEiOiJjajViYW53Z2cwdGxqMnFwMTV2bmVqYnJwIn0.l0j00kH8zTiWMJk0u3TVnw};
 var map = L.map('map').setView([37.8, -96], 4);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
